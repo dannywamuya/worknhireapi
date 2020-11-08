2e2184c8ffc2e55663428a3695be328a4647b301
Danny Wamuya <55041781+dannywamuya@users.noreply.github.com>
2020-11-08 11:27:12 +0300

    Development (#5)
    * Add client, worker and job details classes and corresponding tests
    
    * Danny (#2)
    
    * Deployment config
    
    * Deployment config 2
    
    * Deployment config 3
    
    * Deployment config 4
    
    * Deployment config 5
    
    * Deployment config 6
    
    * Deployment config 7
    
    * Deployment config 8
    
    * Deployed API
    
    Co-authored-by: Danny Wamuya <dannywamuya@gmail.com>
    
    * Create README.md
    
    * Update README.md
    
    * Danny (#4)
    
    * Deployment config
    
    * Deployment config 2
    
    * Deployment config 3
    
    * Deployment config 4
    
    * Deployment config 5
    
    * Deployment config 6
    
    * Deployment config 7
    
    * Deployment config 8
    
    * Deployed API
    
    * Enable CORS 2
    
    Co-authored-by: Danny Wamuya <dannywamuya@gmail.com>
    
    Co-authored-by: Danny Wamuya <dannywamuya@gmail.com>

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -29,6 +29,27 @@ public class App {
         Connection conn;
         Gson gson =new Gson();
 
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers",
+                        accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods",
+                        accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) ->
+            response.header("Access-Control-Allow-Origin", "*")
+        );
+
         //CREATE
         //post: creates a new client
         post("/clients/new", "application/json",(req, res) -> {
